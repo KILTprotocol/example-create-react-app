556829856cadc36e62ddd8009b937b848d07f6c8
Timo Welde <tjwelde@gmail.com>
2021-03-03 18:52:07 +0100

    feat: add kilt and make it work

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import Kilt from '@kiltprotocol/sdk-js'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const mnemonic = Kilt.Identity.generateMnemonic()
+  const identity = Kilt.Identity.buildFromMnemonic(mnemonic)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          {identity.address}
         </p>
         <a
           className="App-link"
